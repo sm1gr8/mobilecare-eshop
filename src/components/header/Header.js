bc08358163b86435109ec22f755376b72ea18d64
rashid3838 <rashidkhan28338@gmail.com>
2023-08-30 19:32:13 +0500

    rashid code push

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,17 +5,17 @@ function Header() {
   return (
     <header>
       {/* Start topbar here  */}
-      <div className="topbar dark-bg pt-1 pb-1">
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-lg-6 col-sm-12">
-              <div className="topbar-content">Pomoc & kontakt</div>
+      <div className='topbar dark-bg pt-1 pb-1'>
+        <div className='container'>
+          <div className='row align-items-center'>
+            <div className='col-lg-6 col-sm-12'>
+              <div className='topbar-content'>Pomoc & kontakt</div>
             </div>
-            <div className="col-lg-6 col-sm-12">
-              <div className="topbar-content d-flex justify-content-end">
-                <div className="address pe-3">Rastislavova 68, Košice</div>
-                <div className="tb-phone">
-                  <NavLink href="tel:+421 919 215 491">+421 919 215 491</NavLink>
+            <div className='col-lg-6 col-sm-12'>
+              <div className='topbar-content d-flex justify-content-end'>
+                <div className='address pe-3'>Rastislavova 68, Košice</div>
+                <div className='tb-phone'>
+                  <NavLink href='tel:+421 919 215 491'>+421 919 215 491</NavLink>
                 </div>
               </div>
             </div>
@@ -24,17 +24,17 @@ function Header() {
       </div>
       {/* Ends topbar here  */}
       {/* Start topbar2 with links here  */}
-      <div className="topbar light-bg pt-1 pb-1">
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-sm-12">
-              <div className="topbar-content d-flex justify-content-end">
+      <div className='topbar light-bg pt-1 pb-1'>
+        <div className='container'>
+          <div className='row align-items-center'>
+            <div className='col-sm-12'>
+              <div className='topbar-content d-flex justify-content-end'>
                 <ul>
-                  <li><NavLink href="#">Sledovanie zásielky</NavLink></li>
-                  <li><NavLink href="#">Vrátenie tovaru</NavLink></li>
-                  <li><NavLink href="#">O nás</NavLink></li>
-                  <li><NavLink href="#">FAQ</NavLink></li>
-                  <li><NavLink href="#">Blog</NavLink></li>
+                  <li><NavLink href='#'>Sledovanie zásielky</NavLink></li>
+                  <li><NavLink href='#'>Vrátenie tovaru</NavLink></li>
+                  <li><NavLink href='#'>O nás</NavLink></li>
+                  <li><NavLink href='#'>FAQ</NavLink></li>
+                  <li><NavLink href='#'>Blog</NavLink></li>
                 </ul>
               </div>
             </div>
@@ -43,12 +43,12 @@ function Header() {
       </div>
       {/* Ends topbar here  */}
       {/* Start topbar Announcement here  */}
-      <div className="topbar-announcement red-bg pt-1 pb-1">
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-sm-12">
-              <div className="topbar-content">
-                <div className="announcement-content d-flex justify-content-center">
+      <div className='topbar-announcement red-bg pt-1 pb-1'>
+        <div className='container'>
+          <div className='row align-items-center'>
+            <div className='col-sm-12'>
+              <div className='topbar-content'>
+                <div className='announcement-content d-flex justify-content-center'>
                   Nové iPhone 14 PRO s 20% zľavou :sparkles:
                 </div>
               </div>
@@ -57,54 +57,54 @@ function Header() {
         </div>
       </div>
       {/* Ends topbar Announcement here  */}
-      <div className="navbar-hold dark-bg pt-3 pb-3 mb-3">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-4">
-              <nav className="navbar navbar-expand-lg navbar-dark">
-                <div className="collapse navbar-collapse justify-content-left">
-                  <ul className="navbar-nav left-side">
-                    <li className="nav-item active">
-                      <NavLink className="nav-link" to='/výpredaj'>
-                        <span class="icon-sale"></span>  Výpredaj
+      <div className='navbar-hold dark-bg pt-3 pb-3 mb-3'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-sm-4'>
+              <nav className='navbar navbar-expand-lg navbar-dark'>
+                <div className='collapse navbar-collapse justify-content-left'>
+                  <ul className='navbar-nav left-side'>
+                    <li className='nav-item active'>
+                      <NavLink className='nav-link' to='/výpredaj'>
+                        <span className='icon-sale'></span>  Výpredaj
                       </NavLink>
                     </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to='/novinky'>
-                        <span class="icon-element-02"></span> Novinky
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' to='/novinky'>
+                        <span className='icon-element-02'></span> Novinky
                       </NavLink>
                     </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to='/mcservis'>
-                        <span class="icon-repair"></span> MC Servis
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' to='/mcservis'>
+                        <span className='icon-repair'></span> MC Servis
                       </NavLink>
                     </li>
                   </ul>
                 </div>
               </nav>
             </div>
-            <div className="col-sm-4">
-              <NavLink className="navbar-brand" to='/'>
-                <img src="images/logo.png" alt="Logo" className="img-fluid" />
+            <div className='col-sm-4'>
+              <NavLink className='navbar-brand' to='/'>
+                <img src='images/logo.png' alt='Logo' className='img-fluid' />
               </NavLink>
             </div>
-            <div className="col-sm-4">
-              <nav className="navbar navbar-expand-lg navbar-dark">
-                <div className="collapse navbar-collapse justify-content-end">
-                  <ul className="navbar-nav right-side">
-                    <li className="nav-item active">
-                      <NavLink className="nav-link" href="#">
-                        <span class="icon-user"></span>
+            <div className='col-sm-4'>
+              <nav className='navbar navbar-expand-lg navbar-dark'>
+                <div className='collapse navbar-collapse justify-content-end'>
+                  <ul className='navbar-nav right-side'>
+                    <li className='nav-item active'>
+                      <NavLink className='nav-link' href='#'>
+                        <span className='icon-user'></span>
                       </NavLink>
                     </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" href="#">
-                        <span class="icon-favourite-border"></span>
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' href='#'>
+                        <span className='icon-favourite-border'></span>
                       </NavLink>
                     </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" href="#">
-                        <span class="icon-shopping-bag-outline"></span>
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' href='#'>
+                        <span className='icon-shopping-bag-outline'></span>
                       </NavLink>
                     </li>
                   </ul>
@@ -112,40 +112,88 @@ function Header() {
               </nav>
             </div>
           </div>
-          <div className="row header-bottom-pd">
-            <div className="col-sm-9 d-flex">
-            <button className='button button-white button-with-icon menu-toggel'>Všetky kategórie <span class="icon-burgermenu"></span>
+          <div className='row header-bottom-pd'>
+            <div className='col-sm-9 d-flex'>
+              <button className='button button-white button-with-icon menu-toggel'>Všetky kategórie <span className='icon-burgermenu'></span>
               </button>
-              <nav className="navbar navbar-expand-lg navbar-dark main-menu">
-                <div className="collapse navbar-collapse justify-content-left">
-                  <ul className="navbar-nav left-side">
-                    <li className="nav-item active">
-                      <NavLink className="nav-link" href="#">
+              <nav className='navbar navbar-expand-lg navbar-dark main-menu'>
+                <div className='collapse navbar-collapse justify-content-left'>
+                  <ul className='navbar-nav left-side'>
+                    <li className='nav-item active dropdown'>
+                      <NavLink className='nav-link dropdown-toggle' id='dropdownMenuButton1' data-bs-toggle='dropdown' aria-expanded='false' href='#'>
                         iPhone
                       </NavLink>
+                      <div className='dropdown-menu' aria-labelledby='dropdownMenuButton1'>
+                        <div className='row custom-dropdown-hold'>
+                          <div className='col-md-2 pd-zero'>
+                            <div className='main-dropdown-holds'>
+                              <span>Apple iPhone</span>
+                              <ul className='item-links'>
+                                <li><a href='#'>iPhone 14 PRO</a></li>
+                                <li><a href='#'>iPhone 14 PRO Max</a></li>
+                                <li><a href='#'>iPhone 14 Plus</a></li>
+                                <li><a href='#'>iPhone 14</a></li>
+                                <li><a href='#'>iPhone 13</a></li>
+                                <li><a href='#'>iPhone 13 Mini</a></li>
+                                <li><a href='#'>iPhone SE 2022</a></li>
+                              </ul>
+                            </div>
+                          </div>
+                          <div className='col-md-2 pd-zero'>
+                            <div className='main-dropdown-holds'>
+                              <span></span>
+                              <ul className='item-links'>
+                                <li><a href='#'>iPhone 12</a></li>
+                                <li><a href='#'>iPhone 12 PRO</a></li>
+                                <li><a href='#'>iPhone X</a></li>
+                                <li><a href='#'>iPhone 8</a></li>
+                              </ul>
+                            </div>
+                          </div>
+                          <div className='col-md-2 pd-zero'></div>
+                          <div className='col-md-3 pd-zero'>
+                            <div className='dropdown-img box-one'>
+                              <div className='content-hold'>
+                                <h2>Nové</h2>
+                                <a className='button-dark' href='#'>Nakupovať <i class="fas fa-angle-right"></i></a>
+                              </div>
+                              <div className='img-hold'><img src='images/dropdown/nav-drop-00.png'></img></div>
+                            </div>
+                          </div>
+                          <div className='col-md-3 pd-zero'>
+                            <div className='dropdown-img box-second'>
+                              <div className='content-hold'>
+                                <h2>Výpredaj</h2>
+                                <a className='button-dark' href='#'>Nakupovať <i class="fas fa-angle-right"></i></a>
+                              </div>
+                              <div className='img-hold'><img src='images/dropdown/nav-drop-01.png'></img></div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" href="#">
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' href='#'>
                         iPad
                       </NavLink>
                     </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" href="#">
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' href='#'>
                         MacBook
                       </NavLink>
                     </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" href="#">
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' href='#'>
                         Apple Airpods
                       </NavLink>
                     </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" href="#">
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' href='#'>
                         Smarthome
                       </NavLink>
                     </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" href="#">
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' href='#'>
                         Príslušenstvo
                       </NavLink>
                     </li>
@@ -153,11 +201,11 @@ function Header() {
                 </div>
               </nav>
             </div>
-            <div className="col-sm-3">
+            <div className='col-sm-3'>
               <div className='header-search-hold'>
-                <div class='easy-autocomplete'>
+                <div className='easy-autocomplete'>
                   <input id='search-on-button-click' placeholder='Zadajte názov produktu'></input>
-                  <span class="icon-search-outline"></span>
+                  <span className='icon-search-outline'></span>
                 </div>
               </div>
             </div>
