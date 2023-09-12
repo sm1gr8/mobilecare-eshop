2cfefd87521c92427a544ddf2d2155ad73f32e90
Rashid28338 <rashidkhan28338@gmail.com>
2023-09-13 01:37:13 +0500

    code push

--- public/js/cutsom.main.js
+++ public/js/cutsom.main.js
@@ -224,11 +224,11 @@ jQuery("#carousel").owlCarousel({
     },
 
     600: {
-      items: 3
+      items: 2
     },
 
     1024: {
-      items: 4
+      items: 2
     },
 
     1366: {
@@ -237,39 +237,7 @@ jQuery("#carousel").owlCarousel({
   }
 });
 
-jQuery("#carousel-second").owlCarousel({
-    autoplay: false,
-    rewind: true, /* use rewind if you don't want loop */
-    margin: 20,
-     /*
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    */
-    responsiveClass: true,
-    autoHeight: true,
-    autoplayTimeout: 7000,
-    smartSpeed: 800,
-    nav: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-  
-      600: {
-        items: 3
-      },
-  
-      1024: {
-        items: 4
-      },
-  
-      1366: {
-        items: 6
-      }
-    }
-  });
 
-  
       var acc = document.getElementsByClassName("accordion");
       var i;
 
