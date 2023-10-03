8882f6813046cc99cc01c2d769e8c1f424ee50ce
Sharjeel Abbas <sharjeelabbas007@gmail.com>
2023-10-03 18:57:50 +0500

    sharjeel add shake effect

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,17 +1,25 @@
 import React from 'react';
+// import 'owl.carousel/dist/assets/owl.carousel.css';
+// import 'owl.carousel';
+// import $ from 'jquery';
 import './Slider.css';
+
+
 function Slider() {
+
+
+
   return (
     <div>
       <div className="main-slider-hold">
-      <div class="owl-carousel owl-carousel-1">
+        <div class="owl-carousel owl-carousel-1">
           <div class="item">
             <div className='middle-slider one-slider-h'>
               <div className='box-content'>
                 <small>iPhone 17 X-PRO</small>
                 <h2>Nový iPhone
                   naskladnený!</h2>
-                <a href=''>Kúpiť</a>
+                <a href='javascript:;'>Kúpiť</a>
               </div>
               <div className='box-img-holds'>
                 <div className='price-tag-head' style={{ backgroundImage: 'url("./images/price-tag.svg")' }}><span className='price-tag'>188€</span></div>
@@ -24,10 +32,10 @@ function Slider() {
                 <small>iPhone 17 X-PRO</small>
                 <h2>Nový iPhone
                   naskladnený!</h2>
-                <a href=''>Kúpiť</a>
+                <a href='javascript:;'>Kúpiť</a>
               </div>
               <div className='box-img-holds'>
-              <div className='price-tag-head-second' style={{ backgroundImage: 'url("./images/price-tag.svg")' }}><span className='price-tag'>899€</span></div>
+                <div className='price-tag-head-second' style={{ backgroundImage: 'url("./images/price-tag.svg")' }}><span className='price-tag'>899€</span></div>
                 <img src='images/mobile-ss.png'></img></div>
             </div>
           </div>
@@ -37,7 +45,7 @@ function Slider() {
                 <small>MacBook PRO M2</small>
                 <h2>MacBooky
                   na sklade!</h2>
-                <a href=''>Kúpiť</a>
+                <a href='javascript:;'>Kúpiť</a>
               </div>
               <div className='box-img-holds'><img src='images/macbook.png'></img></div>
             </div>
